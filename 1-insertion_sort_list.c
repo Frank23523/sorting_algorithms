b66797265a7df56931c88e6a277b48a0aa7a9778
Frank Anin <frankanin99@gmail.com>
2024-02-24 19:41:38 +0000

    insertion sort and its time complexity

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,30 +3,27 @@
 void swap_node_position(listint_t **head, listint_t **node1, listint_t *node2);
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
+ * insertion_sort_list - sorts dl list inascending order with Bubble sort
  * @list: A pointer to the head of a doubly-linked list of integers.
- *
- * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *unsorted_node, *sorted_node, *temp;
+	listint_t *unsorted_node, *sorted_node, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    for (unsorted_node = (*list)->next; unsorted_node != NULL; unsorted_node = temp)
-    {
-        temp = unsorted_node->next;
+	for (unsorted_node = (*list)->next; unsorted_node; unsorted_node = temp)
+	{
+		temp = unsorted_node->next;
 
-        sorted_node = unsorted_node->prev;
-        while (sorted_node != NULL && unsorted_node->n < sorted_node->n)
-        {
-            swap_node_position(list, &sorted_node, unsorted_node);
-            print_list(*list);
-        }
-    }
+		sorted_node = unsorted_node->prev;
+		while (sorted_node != NULL && unsorted_node->n < sorted_node->n)
+		{
+			swap_node_position(list, &sorted_node, unsorted_node);
+			print_list(*list);
+		}
+	}
 }
 
 /**
@@ -37,17 +34,17 @@ void insertion_sort_list(listint_t **list)
  */
 void swap_node_position(listint_t **head, listint_t **node1, listint_t *node2)
 {
-    (*node1)->next = node2->next;
-    if (node2->next != NULL)
-        node2->next->prev = *node1;
-    node2->next = *node1;
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->next = *node1;
 
-    node2->prev = (*node1)->prev;
-    if ((*node1)->prev != NULL)
-        (*node1)->prev->next = node2;
-    else
-        *head = node2;
-    (*node1)->prev = node2;
+	node2->prev = (*node1)->prev;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
 
-    *node1 = node2->prev;
+	*node1 = node2->prev;
 }
